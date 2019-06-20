f9c87e6123bd6523df312b5592e6e9e05c08d606
Reyaad Rafeek <reyaad510@gmail.com>
2019-06-19 22:08:36 -0400

    Repair function and testing completed

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,16 @@
 const enhancer = require('./enhancer.js');
 
+describe('the enhancer', () => {
+
+    describe('the repair function', () => {
+        it('should restore the durability to 100', () => {
+
+            // arrange
+            const weapon = {};
+            // act
+            const result = enhancer.repair(weapon); 
+            // assert
+            expect(result.durability).toBe(100);
+        })
+    })
+})
